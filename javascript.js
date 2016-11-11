b23ab22fd31826baf243ce8c436bc7b4f1c2997a
Andrealiz Afable <dreafable@gmail.com>
2016-11-10 19:09:43 -0800

    add changing colors to box

--- javascript.js
+++ javascript.js
@@ -1,3 +1,40 @@
+var clickBoxOne = (function() {
+  var colors = ["red", "orange", "green"];
+    var index = 0;
+    return function() {
+    document.getElementById('boxOne').style.background = colors[index%3];
+      index++;
+    }
+})();
+
+var clickBoxTwo = (function() {
+  var colors = ["red", "orange", "green"];
+    var index = 0;
+    return function() {
+    document.getElementById('boxTwo').style.background = colors[index%3];
+      index++;
+    }
+})();
+
+
+// var clickBoxOne = (function() {
+//   var counter = 0;
+//   return function() {
+//     counter++;
+//     return console.log("Box One clicked " + counter + " times.");
+//   }
+// })();
+
+// var clickBoxTwo = (function() {
+//   var counter = 0;
+//   return function() {
+//     counter++;
+//     return console.log("Box One clicked " + counter + " times.");
+//   }
+// })();
+
+
+
 function createAccount() {
   var firstName = document.getElementById('firstName');
   var lastName = document.getElementById('lastName');
